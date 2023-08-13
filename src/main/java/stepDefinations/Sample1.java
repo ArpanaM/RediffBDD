cdd6a91f63b39602bd94486294a3c915a3f5f72e
NAGESH <NAGESH@LENOVO>
2023-08-13 08:55:56 +0530

    New class is created...

---
+++ src/main/java/stepDefinations/Sample1.java
@@ -0,0 +1,10 @@
+package stepDefinations;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("this class is newly created...");
+	}
+
+}
