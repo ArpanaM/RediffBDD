5786d865e2d1fa258a1764ef4aa3584b91e86ac4
NAGESH <NAGESH@LENOVO>
2023-08-13 08:59:23 +0530

    updated...

--- src/main/java/stepDefinations/Sample1.java
+++ src/main/java/stepDefinations/Sample1.java
@@ -5,6 +5,7 @@ public class Sample1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("this class is newly created...");
+			System.out.println("this file is updated...");
 	}
 
 }
